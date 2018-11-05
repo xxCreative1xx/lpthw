396a642640326922e037f370c3aba2eb16fd1b0f
Alex Hagerman <alex@unexpectedeof.net>
2018-11-05 04:28:03 -0500

    Breaking per request.

--- ex8.py
+++ ex8.py
@@ -2,11 +2,11 @@ formatter = "{} {} {} {}"
 
 print(formatter.format(1, 2, 3, 4))
 print(formatter.format("one", "two", "three", "four"))
-print(formatter.format(True, False, False, True))
+print(formatter.format(true, false, false, true))
 print(formatter.format(formatter, formatter, formatter, formatter))
 print(formatter.format(
     "Try your",
-    "Own text here",
-    "Maybe a poem",
-    "Or a song about fear"
+    "\nOwn text here",
+    "Maybe a poem\n",
+    "Or a song\n about fear"
 ))
